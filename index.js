f3caf2d946c360c1be8883e00d609761760e2835
xiangcaozhu <315731263@qq.com>
2018-09-23 23:04:59 +0800

    art-template precompile plugin

---
+++ index.js
@@ -0,0 +1,87 @@
+
+const path = require('path');
+const template = require('art-template');
+const precompile = requier('art-template/lib/precompile');
+const _ = fis.util;
+/**
+ * Compile 阶段插件接口
+ * @param  {string} content     文件内容
+ * @param  {File}   file        fis 的 File 对象 [fis3/lib/file.js]
+ * @param  {object} settings    插件配置属性
+ * @return {string}             处理后的文件内容
+ */
+module.exports = function (content, file, settings) {
+    // 不处理空文件，处理空文件有人反馈报错。
+    if (!content || !content.trim() || file.basename[0] === '_') {
+        return content;
+    }
+    // 初始化配置
+    let result;
+    // 默认与自定义配置合并
+    let options = _.assign({
+        filename: path.basename(file.realpath),
+        syncImport: true,
+        relativeUrls: true
+    }, settings);
+    // 自定义minimize存在，就用自定义的
+    if (options.minimize) {
+        template.defaults.minimize = options.minimize;
+    }
+    // 自定义compileDebug存在，就用自定义的
+    if (options.compileDebug) {
+        template.defaults.compileDebug = options.compileDebug;
+    }
+    // 自定义escape存在，就用自定义的
+    if (options.escape) {
+        template.defaults.escape = options.escape;
+    }
+    // 自定义cache存在，就用自定义的
+    if (options.cache) {
+        template.defaults.cache = options.cache;
+    }
+    // 自定义sourceMap存在，就用自定义的
+    // 是否产生map源文件
+    let sourceMap = options.sourceMap;
+    let sourceMapFile;
+    if (sourceMap) {
+        if (_.isBoolean(sourceMap)) {
+            sourceMap = {};
+        }
+        let sourceMapPath = file.realpath + '.map';
+        sourceMapFile = fis.file.wrap(sourceMapPath);
+        sourceMapFile.setContent('');
+        options.sourceMap = _.assign({
+            outputSourceFiles: true,
+            sourceMapURL: path.basename(sourceMapFile.url),
+            sourceMapBasepath: fis.project.getProjectPath(),
+            sourceMapRootpath: '/source',
+            sourceMapFileInline: false
+        }, sourceMap);
+    }
+    // 默认解析规则输入
+    options.rules.push(...template.defaults.rules);
+    options.ignore.push(`require`);
+    // content是fis3读取的文件内容
+    options.source = content;
+    options.filename = file.filename;
+    options.sourceRoot = process.cwd();
+    // 预编译模板文件
+    try {
+        result = precompile(options);
+    } catch (error) {
+        throw error;
+    }
+    // art-template编译出来的源文件
+    const templateCode = result.toString();
+    // art-template编译产出的映射文件
+    const sourceMapResult = result.sourceMap;
+    // 未知规则
+    // const ast = result.ast;
+    // 源文件数据处理
+    if (sourceMapFile && sourceMapResult) {
+        sourceMapFile.setContent(sourceMapResult.toString());
+        file.derived.push(sourceMapFile);
+    }
+    // 把处理结果返回
+    return templateCode;
+};
---
+++ readme.md
@@ -0,0 +1,27 @@
+# fis3-parser-prcompile-art-template
+根据官方最新的art-template@4.x内置的recompile方法，借鉴官方的art-template-loader和fis3官方插件规范编写。
+## 预编译
+通过这个插件，把`.tpl`或`.art`等自定义的模板文件，编译成js文件，如果`fis.match()`中设置了`isMod:true`,将编译产出的文件按照commonjs规范，导出编译后的渲染函数，如：
+``` js
+module.exports = function () {}
+```
+再经过fis3的自动包裹成符合`mod.js`模块加载器的前端规范：
+``` js
+define('src/components/table.tpl,function(require, exports, module){
+    module.exports = function () {}
+})
+```
+## 使用
+
+> npm install fis3-parser-prcompile-art-template
+
+在需要引入的页面中，引入模板文件,这样做的目的是，在浏览器中，可以不依赖与`art-template`库，直接在浏览器中运行，相比于使用`template-web.js`在浏览器中运行时编译，提高性能，特别是在ie8这样的低版本浏览器，效果更明显。
+``` js
+var renderTable = require('../../components/table/table.tpl);
+// 返回的直接就是渲染函数
+var tableHtml = renderTable({
+    title:'表格标题'
+})
+// 插入到文档页面中
+$('body).append(tableHtml);
+```
